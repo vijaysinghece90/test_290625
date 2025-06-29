548383c2ab61193e0e4d25673571bb4fff28df95
Vijay 9. Singh (EXT-Nokia) <vijay.9.singh.ext@nokia.com>
2025-06-29 11:30:46 +0530

    3rd-commit

--- environment/prod/main.tf
+++ environment/prod/main.tf
@@ -1,5 +1,6 @@
 # We strongly recommend using the required_providers block to set the
-
+# provider version to a specific version or range of versions.
+# This ensures that your configuration will always use the same version of the provider,
 terraform {
   required_providers {
     azurerm = {
